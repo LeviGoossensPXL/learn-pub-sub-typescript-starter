ddc2065e2280d70338885b015ef6bd9acaf1fd16
levig <levi.goossens107@gmail.com>
2026-09-21 19:22:07 +0200

    connect to rabbitMQ server

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,16 @@
+import amqp from "amqplib";
+
 async function main() {
   console.log("Starting Peril server...");
+  const connString = "amqp://guest:guest@localhost:5672/";
+  const conn = await amqp.connect(connString);
+  console.log("Connnection was succesful.");
+
+  process.on('SIGINT', async () => {
+    await conn.close();
+    console.log('Connection closed.');
+    console.log('Program is shuting down.');
+  });
 }
 
 main().catch((err) => {
