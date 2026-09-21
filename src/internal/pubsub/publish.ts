6d4f8d938104e69c7a3a313d94c7de73199bd6e5
levig <levi.goossens107@gmail.com>
2026-09-21 20:39:23 +0200

    publish first json message

---
+++ src/internal/pubsub/publish.ts
@@ -0,0 +1,28 @@
+import type { ConfirmChannel } from "amqplib";
+
+export function publishJSON<T>(
+  ch: ConfirmChannel,
+  exchange: string,
+  routingKey: string,
+  value: T,
+): Promise<void> {
+    const buffer = Buffer.from(JSON.stringify(value))
+
+    return new Promise((resolve, reject) => {
+    ch.publish(
+      exchange,
+      routingKey,
+      buffer,
+      { contentType: "application/json" },
+
+      (err) => {
+        if (err) {
+          reject(err);
+        } else {
+          resolve();
+        }
+
+      },
+    );
+  });
+};
--- src/server/index.ts
+++ src/server/index.ts
@@ -1,10 +1,17 @@
 import amqp from "amqplib";
+import { publishJSON } from "../internal/pubsub/publish.js";
+import { ExchangePerilDirect, PauseKey } from "../internal/routing/routing.js";
+import { GameState, type PlayingState } from "../internal/gamelogic/gamestate.js";
 
 async function main() {
   console.log("Starting Peril server...");
   const connString = "amqp://guest:guest@localhost:5672/";
   const conn = await amqp.connect(connString);
   console.log("Connnection was succesful.");
+
+  const channel = await conn.createConfirmChannel();
+  const playingState: PlayingState = {isPaused: true}
+  publishJSON(channel, ExchangePerilDirect, PauseKey, playingState)
 
   process.on('SIGINT', async () => {
     await conn.close();
